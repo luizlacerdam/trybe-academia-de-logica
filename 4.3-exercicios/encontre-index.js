69c4ef44ca5c7f1472c1f400953e9c68587c466f
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-07-22 13:24:42 -0300

    exercicio-1

---
+++ 4.3-exercicios/encontre-index.js
@@ -0,0 +1,8 @@
+let numbers = [9, 15, 12, 27, 11, 51, 59];
+let index = 10;
+
+if (numbers[index] == undefined) {
+    console.log('Elemento não encontrado no array.');
+} else {
+    console.log(numbers[index]);
+}
