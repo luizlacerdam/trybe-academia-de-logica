b90a3788782bf1d0a9bb9fc462abb5dbafd0db76
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-15 13:43:55 -0300

    exercicio-completo-refatorado

--- 7.2-exercicio/script.js
+++ 7.2-exercicio/script.js
@@ -20,13 +20,17 @@ console.log(tipo('hello'));
 
 // Exercício 4: Escreva uma função que receba um nome de usuário e retorne a seguinte mensagem: "Boas vindas, USERNAME!". Valide que o tipo de dado seja string e o texto tenha pelo menos 1 caracter, e caso o parâmetro não atenda alguma das condições o retorno deve ser a string "Parâmetro inválido.".
 
-const boasVindas = (usuario) => {
-  if (typeof usuario === 'string' && usuario.length > 0) {
-    return `Boas vindas, ${usuario}!`;
-  }
-  return 'Parâmetro inválido.';
-}
-console.log(boasVindas(1));
+const boasVindas = (usuario) => (typeof usuario === 'string' && usuario.length > 0)?`Boas vindas, ${usuario}!`:'Parâmetro inválido.';
+
+console.log(boasVindas('Joseane'));
+
+// const boasVindas = (usuario) => {
+//   if (typeof usuario === 'string' && usuario.length > 0) {
+//     return `Boas vindas, ${usuario}!`;
+//   }
+//   return 'Parâmetro inválido.';
+// }
+// console.log(boasVindas(1));
 
 // Exercício 5: Escreva uma função que calcule a velocidade final de um objeto em queda livre dado o tempo que levou para atingir o solo. A fórmula para o cálculo é velocidade = tempo * gravidade. Considere a gravidade constante de 9.8.
 
