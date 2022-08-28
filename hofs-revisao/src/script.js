69b68cc611c80b69d2330c29325091331977417a
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-28 18:27:47 -0300

    requisito-1

--- hofs-revisao/src/script.js
+++ hofs-revisao/src/script.js
@@ -1,1 +1,9 @@
+const { fighters } = require('./data.js');
 const data = require('./data.js');
+
+// requisito 1
+const combatExp = (obj) => {
+  const lutadoresSorted = obj.fighters.sort((a,b) => a.age - b.age);
+  return `${lutadoresSorted[0].name} possui maestria. ${lutadoresSorted[lutadoresSorted.length - 1].name} ainda é um pupilo.`;
+};
+console.log(combatExp(data));
