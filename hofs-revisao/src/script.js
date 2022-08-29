d0b2f8ba8ad3ea2bcb2797e51c5db8c03ec8b1b3
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-29 09:36:01 -0300

    requisito-2

--- hofs-revisao/src/script.js
+++ hofs-revisao/src/script.js
@@ -1,9 +1,25 @@
-const { fighters } = require('./data.js');
 const data = require('./data.js');
-
+const { fighters } = require('./data.js');
 // requisito 1
 const combatExp = (obj) => {
-  const lutadoresSorted = obj.fighters.sort((a,b) => a.age - b.age);
+  const lutadoresSorted = obj.fighters.sort((a,b) => b.age - a.age);
   return `${lutadoresSorted[0].name} possui maestria. ${lutadoresSorted[lutadoresSorted.length - 1].name} ainda é um pupilo.`;
 };
-console.log(combatExp(data));
+
+// requisito 2
+const mediaIdadeClasse = ({classes, fighters}) => {
+  return Object.keys(classes).map((element) => {
+    const id = classes[element];
+    const idadeFighter = fighters.filter((lutador) => lutador.class === id).map((element) => element.age);
+    let total = 0; 
+    idadeFighter.forEach(element => {
+      total += element;
+    });
+    const media = Math.floor(total / idadeFighter.length);
+    return {
+      [element]: media,
+    }
+  });
+};
+
+console.log(mediaIdadeClasse(data));
