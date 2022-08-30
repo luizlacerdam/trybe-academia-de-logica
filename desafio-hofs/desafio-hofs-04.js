5a307755c60d836d053680dca96ff8bf6b1fcb97
luizlacerdam <luizlacerdam.dev@gmail.com>
2022-08-30 09:03:43 -0300

    exercicio-4

---
+++ desafio-hofs/desafio-hofs-04.js
@@ -0,0 +1,10 @@
+const data = require('./data');
+
+// 4 - Filtre e retorne um array apenas com o nome das bandas que são dos Estados Unidos
+
+const usaFilter = () => {
+  const bandas = data.bands;
+  return bandas.filter((elemento) => elemento.country === 'United States')
+}
+console.log(usaFilter());
+module.exports = usaFilter;
